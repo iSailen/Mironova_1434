432c3f6b6a590c927e43814f99cf471465bb8e76
iSailen <73995191+iSailen@users.noreply.github.com>
2020-11-08 18:08:03 +0300

    Create create Main.java
    Taska 1.3

---
+++ create Main.java
@@ -0,0 +1,13 @@
+/* Объяви переменные a, b типа int и переменную s типа String.
+Сразу же в строке объявления присвой им любые разные значения. */
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+        String s = "4";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(s);
+    }
+}
